3d3383376754bd7e5f5d1cd5cad39552e533116c
Krystofr <krystofrnanju@gmail.com>
2022-07-07 08:59:35 +0100

    Created Note Row and populated LazyColumn with dummy data from custom list.

--- app/src/main/java/app/christopher/jetnote/MainActivity.kt
+++ app/src/main/java/app/christopher/jetnote/MainActivity.kt
@@ -1,8 +1,10 @@
 package app.christopher.jetnote
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,10 +12,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import app.christopher.jetnote.data.NoteDataSource
 import app.christopher.jetnote.screen.NoteScreen
 import app.christopher.jetnote.ui.theme.JetNoteTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,13 +25,14 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
+                    NoteScreen(notes = NoteDataSource().loadNotes(), onAddNote = {}, onRemoveNote = {})
                 }
             }
         }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
--- app/src/main/java/app/christopher/jetnote/screen/NoteScreen.kt
+++ app/src/main/java/app/christopher/jetnote/screen/NoteScreen.kt
@@ -1,17 +1,21 @@
 package app.christopher.jetnote.screen
 
-import android.content.Context
+import android.os.Build
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -20,8 +24,11 @@ import androidx.compose.ui.unit.dp
 import app.christopher.jetnote.R
 import app.christopher.jetnote.components.NoteButton
 import app.christopher.jetnote.components.NoteInputText
+import app.christopher.jetnote.data.NoteDataSource
 import app.christopher.jetnote.model.Note
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NoteScreen(
     notes: List<Note>,
@@ -48,6 +55,7 @@ fun NoteScreen(
         //Content
         Column(Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.size(120.dp))
             //Call NoteInputText composable
             NoteInputText(text = title, label = "Title", onTextChange = {
                 if (it.all { char ->
@@ -74,11 +82,49 @@ fun NoteScreen(
                 }
             })
         }
+        Divider(Modifier.padding(15.dp))
+        LazyColumn {
+            items(notes) { note ->
+                NoteRow(note = note, onNoteClicked = {})
+            }
+        }
     }
 }
 
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun NoteRow(
+    modifier: Modifier = Modifier,
+    note: Note,
+    onNoteClicked: (Note) -> Unit) {
+    Surface(modifier
+        .padding(14.dp)
+        .clip(RoundedCornerShape(topEnd = 33.dp, bottomStart = 33.dp))
+        .fillMaxWidth(),
+        color = Color(0xFFDFE6EB),
+        elevation = 10.dp) {
+        Column(modifier
+            .clickable { onNoteClicked(note) }
+            .padding(horizontal = 14.dp, vertical = 6.dp),
+            horizontalAlignment = Alignment.Start) {
+            Text(text = note.title!!,
+                style = MaterialTheme.typography.subtitle2)
+            Text(text = note.description!!, style = MaterialTheme.typography.subtitle1)
+            Text(text = note.dateCreated.format(DateTimeFormatter.ofPattern("EEE, d MMM")),
+                style = MaterialTheme.typography.caption, color = MaterialTheme.colors.primary)
+
+
+        }
+
+
+    }
+
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
 fun NoteScreenPreview() {
-    NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
+    NoteScreen(notes = NoteDataSource().loadNotes(), onAddNote = {}, onRemoveNote = {})
 }
